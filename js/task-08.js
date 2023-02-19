cded54b6cbbf005c538dc2a0c8fad7b91cb5307d
Lebedynskyi Roman <cliffcrystall@gmail.com>
2023-02-19 23:25:01 +0100

    DOM to practice: homework task-08 completed

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,34 @@
+// Обробка відправлення форми
+// form.login - form повинна відбуватися відповідно до події submit.
+// Під час відправлення форми сторінка не повинна перезавантажуватися.
+// Якщо у формі є незаповнені поля, виводь alert з попередженням про те,
+// що всі поля повинні бути заповнені.
+// Якщо користувач заповнив усі поля і відправив форму,
+//     збери значення полів в об'єкт, де ім'я поля буде ім'ям властивості,
+// а значення поля - значенням властивості.Для доступу до елементів форми використовуй властивість elements.
+// Виведи об'єкт із введеними даними в консоль
+// і очисти значення полів форми методом reset.
 
+const formRef = document.querySelector(".login-form");
+
+formRef.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const { email, password } = event.currentTarget.elements;
+  if (email.value === "" || password.value === "") {
+    alert("You should fill all fields, dude!!!");
+  }
+  const userInputDataObj = new Object();
+  userInputDataObj.email = email.value;
+  userInputDataObj.password = password.value;
+  console.log(userInputDataObj);
+});
+
+resetInputData();
+
+function resetInputData() {
+  formRef.addEventListener("submit", (event) => {
+    const { email, password } = event.currentTarget.elements;
+    email.value = "";
+    password.value = "";
+  });
+}
