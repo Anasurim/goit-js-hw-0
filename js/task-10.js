6965a19c718222c5158eaa5b4f4e239e209b7811
Lebedynskyi Roman <cliffcrystall@gmail.com>
2023-02-20 13:02:04 +0100

    DOM to practice: homework task-10 in process: can create boxes with random hex

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,34 @@
+// Створи функцію createBoxes(amount),
+// яка приймає один параметр - число.Функція створює стільки < div >,
+// скільки вказано в amount і додає їх у div#boxes.
+
+// Розміри найпершого <div> - 30px на 30px.
+// Кожен елемент після першого повинен бути ширшим
+// і вищим від попереднього на 10px.
+// Всі елементи повинні мати випадковий колір фону
+// у форматі HEX.Використовуй готову
+// функцію getRandomHexColor для отримання кольору.
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const createBtnRef = document.querySelector("button[data-create]");
+const inputRef = document.querySelector("input");
+const divBoxRef = document.querySelector("#boxes");
+const boxRef = document.querySelector(".box_item");
+
+createBoxes();
+
+function createBoxes(amount) {
+  inputRef.addEventListener("input", (event) => {
+    amount = event.currentTarget.value;
+
+    createBtnRef.addEventListener("click", () => {
+      divBoxRef.insertAdjacentHTML(
+        "afterbegin",
+        `<div style="width: 30px; height: 30px; background-color: ${getRandomHexColor()}" class="box_item"></div>`
+      );
+    });
+  });
 }
